56e57ce29d16f54d94fbca51862ae23893df59da
Brett Oberg <brett.oberg8@gmail.com>
2020-09-12 16:42:34 -0500

    Todo Service Updates & Unit Tests (#5)

--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -16,7 +16,7 @@ const todos = [
 /**
  * Get a new todo with the provided value.
  * 
- * @param {String} value 
+ * @param {String} value value of the new todo item
  * @returns {{id: String, value: String, isComplete: Boolean}} new todo item
  */
 const getNewTodo = (value = "") => {
@@ -28,17 +28,26 @@ const getNewTodo = (value = "") => {
 /**
  * Get a list of random todo items.
  * 
+ * Note: To simulate a real network request I have included an asynchronous timeout.
+ * 
  * @param {Number} numTodos number of random todos to return
  * @returns {[{id: String, value: String, isComplete: Boolean}]} list of todo items
  */
 const getRandomTodos = async (numTodos = 5) => {
+  if (isNaN(numTodos)) return []
+
   const items = [];
+
   for (let i = 0; i < numTodos; i++) {
     const value = todos[Math.floor(Math.random() * todos.length)];
     items.push(getNewTodo(value))
   }
+
+  // Simulating a real network request that takes time to
+  // fetch and receive data.
   await new Promise((cb) => setTimeout(cb, 2000))
+
   return items
 }
 
-export { getNewTodo, getRandomTodos };
+export { getNewTodo, getRandomTodos, todos };
---
+++ tests/services/TodoService.spec.js
@@ -0,0 +1,112 @@
+import { validate } from 'uuid'
+
+import { getNewTodo, getRandomTodos, todos } from '../../src/services/TodoService';
+
+describe('getNewTodo', () => {
+  describe('when value input is not provided', () => {
+    let actualTodo;
+
+    beforeEach(() => {
+      actualTodo = getNewTodo();
+    })
+
+    test('should return new todo with valid uuid as id', () => {
+      expect(validate(actualTodo.id)).toEqual(true)
+    })
+
+    test('should return new todo with empty string as value', () => {
+      expect(actualTodo.value).toEqual('')
+    })
+
+    test('should return new todo as not completed', () => {
+      expect(actualTodo.isComplete).toEqual(false)
+    })
+  })
+
+  describe('when value input is provided', () => {
+    let actualTodo;
+    const value = 'Test todo';
+
+    beforeEach(() => {
+      actualTodo = getNewTodo(value);
+    })
+
+    test('should return new todo with valid uuid as id', () => {
+      expect(validate(actualTodo.id)).toEqual(true)
+    })
+
+    test('should return new todo with empty string as value', () => {
+      expect(actualTodo.value).toEqual(value)
+    })
+
+    test('should return new todo as not completed', () => {
+      expect(actualTodo.isComplete).toEqual(false)
+    })
+  })
+})
+
+describe('getRandomTodos', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  })
+  describe('when numTodos is not provided', () => {
+    let actualTodoList;
+    const defaultNumTodos = 5;
+
+    beforeEach(async () => {
+      getRandomTodos().then((data) => {
+        actualTodoList = data
+      })
+      jest.runAllTimers()
+    })
+
+    test(`should return ${defaultNumTodos} todos`, () => {
+      expect(Array.isArray(actualTodoList)).toEqual(true)
+      expect(actualTodoList.length).toEqual(defaultNumTodos)
+    })
+
+    test('should return todos that are in the todo list', () => {
+      actualTodoList.forEach(todoItem => {
+        const item = todos.find((el) => el === todoItem.value)
+        expect(item).not.toBeUndefined()
+      })
+    })
+  })
+
+  describe('when numTodos is provided as valid number', () => {
+    let actualTodoList;
+    const numTodos = 3;
+
+    beforeEach(async () => {
+      getRandomTodos(numTodos).then((data) => {
+        actualTodoList = data
+      })
+      jest.runAllTimers()
+    })
+
+    test(`should return ${numTodos} todos`, () => {
+      expect(Array.isArray(actualTodoList)).toEqual(true)
+      expect(actualTodoList.length).toEqual(numTodos)
+    })
+
+    test('should return todos that are in the todo list', () => {
+      actualTodoList.forEach(todoItem => {
+        const item = todos.find((el) => el === todoItem.value)
+        expect(item).not.toBeUndefined()
+      })
+    })
+  })
+
+  describe('when numTodos is provided as not a valid number', () => {
+    let actualTodoList;
+
+    beforeEach(async () => {
+      actualTodoList = await getRandomTodos("invalid")
+    })
+
+    test('should return empty array', () => {
+      expect(Array.isArray(actualTodoList)).toEqual(true)
+      expect(actualTodoList.length).toEqual(0)
+    })
+  })
+})
